f337170e171761ead8b62812820a9519f6dc9828
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-05 16:21:31 +0200

    ADD- successful calculation in console.log

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 
 const btnDomElement = document.getElementById('btn-price')
 
-/*  - recuperare l'input */
+
+
+/* Collegare il bottone al click */
+btnDomElement.addEventListener('click', function () {
+    /*  - recuperare l'input */
 
 const inputDomElement = document.getElementById('kmrequest'); 
 
@@ -12,15 +16,13 @@ const selectDomElement = document.getElementById('agerequest');
 
 	// recupero dall'input il valore 
 	let km = parseInt(inputDomElement.value)
-	
 
     // recupero dalla select il valore 
 
     let age = parseInt(selectDomElement.value)
 
+    /*calcolo*/
 const prezzoTotaleKM = 0.21 * km
-    
-
 let valoreSconto = 0 
 
 if (age === 0) {
@@ -29,23 +31,17 @@ if (age === 0) {
 else if (age === 2) {
     valoreSconto = prezzoTotaleKM * ( 40 / 100)
 }
-
-/* Collegare il bottone al click */
-btnDomElement.addEventListener('click', function () {
+    
+let prezzoFinale = prezzoTotaleKM - valoreSconto; 
+    prezzoFinale = prezzoFinale.toFixed(2);
     
     console.dir(km)
     console.dir(age) 
     console.log(prezzoTotaleKM)
     console.log(valoreSconto)
-
-
-    /*    - calcolare il prezzo del biglietto */
-    
-        let prezzoFinale = prezzoTotaleKM - valoreSconto; 
-    prezzoFinale = prezzoFinale.toFixed(2);
     console.log(prezzoFinale)
-    
+
     })
 
 
-
+    
